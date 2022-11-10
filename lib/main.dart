12b8d6aa83de3281f84c6d774bbe51d9cacc22bd
Jibin K John <jibinkj009@hotmail.com>
2022-11-10 22:44:24 +0530

    init

--- lib/main.dart
+++ lib/main.dart
@@ -108,6 +108,7 @@ class _MyHomePageState extends State<MyHomePage> {
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
         tooltip: 'Increment',
+        backgroundColor: Color(0xff3636A4),
         child: const Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
---
+++ lib/utils/appColors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primaryColor = Color(0xff3636A4);
+  static const Color textDarkColor = Color(0xff000000);
+  static const Color textLightColor = Color(0xffffffff);
+  static const Color greenColor = Color(0xff0F9D58);
+  static const Color redColor = Color(0xffDB4437);
+}
